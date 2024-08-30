b442c543ab85e9ae7879840b141e0ef44ea15bab
belsaadi houari <stat1401@gmail.com>
2024-08-30 23:18:28 +0100

    er

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,8 +10,8 @@ import Signin from './components/login/signin.jsx';
 import Signup from './components/signup/signup.jsx';
 import About from './pages/about/about.jsx';
 import Articles from "./pages/articles/articles.jsx"
-import EmailConfirm from './components/confirmationemail/emailConfirm.js';
-import EmailConfirm2 from './components/confirmationemail2/emailConfirm2.js';
+import EmailConfirm from './components/confirmationemail/emailConfirm.jsx';
+import EmailConfirm2 from './components/confirmationemail2/emailConfirm2.jsx';
 
 import { AuthProvider } from './context/AuthContext.jsx';
 const router = createBrowserRouter([
